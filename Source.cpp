05a00afb425f47b6f34cb9593973ed16639ce8a8
charlieironside <109934381+charlieironside@users.noreply.github.com>
2022-09-30 19:11:26 +1300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -23,8 +23,6 @@ using namespace functions;
 #define _simWidth 15
 #define _simHeight 8.4
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 bool ImGui_ImplOpenGL3_Init(const char* glsl_version);
 
 int scrnX, scrnY;
@@ -32,11 +30,11 @@ const int SCRN_WIDTH = 1920, SCRN_HEIGHT = 1080;
 bool firstMouse = true, addPlanetClicked;
 float lastX = 0, lastY = 0;
 float radius = 1;
-CameraClass camera(glm::vec3(0, 0, 1));
-glm::vec2 normPlacedPosition;
+cam camera(glm::vec3(0, 0, 1));
 
 float deltaTime, lastFrame = 0, currentFrame;
 bool planetsExist = false;
+bool showVectors = true;
 float gravity = 0.05;
 float mouseGradient;
 
@@ -66,14 +64,14 @@ void addPlanet(std::vector<planet>&p, planet toAdd) {
 }
 
 int main() {
-	// Setup
+	// glfw setup
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	// disable window resize
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-	// Remove frap cap
+	// remove frap cap
 	glfwSwapInterval(0);
 
 	// Create window object
@@ -84,7 +82,8 @@ int main() {
 		return -1;
 	}
 
-	// Generate new triangles
+	// vertexData are vertices for a tetrahedron
+	// split each face into 4 smaller triangles using triangleGeneration()
 	for (int i = 0; i < 36; i += 9) {
 		std::vector<float>temp;
 
@@ -101,15 +100,13 @@ int main() {
 		triangleGeneration(temp, vertices, 4);
 	}
 
+	// with the tetrahedron having multiple triangles per face, normalize each vertex
+	// which makes each vertice an equal distance from the center creating a circle
 	circleGeneration(vertices);
 
 	glfwMakeContextCurrent(window);
 	// Input functions
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	glfwSetCursorPosCallback(window, mouse_callback);
-
-	// Hides cursor
-	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	
 
 	// Loads Glad.c
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
@@ -124,10 +121,13 @@ int main() {
 	Shader shaders("vertex.GLSL", "fragment.GLSL");
 	Shader arrowShader("vertex.GLSL", "fragment.GLSL");
 
+	// VBO is a buffer containing vertex data generated above to be sent to the GPU to render
+	// VAO tells the GPU how to intepret/use that data
 	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 
+	// link the vbo to an array buffer
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
 	
@@ -135,6 +135,7 @@ int main() {
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	// seperate VAO and VBO for rendering the vectors/arrows
 	unsigned int VAO2, VBO2;
 	glGenVertexArrays(1, &VAO2);
 	glGenBuffers(1, &VBO2);
@@ -154,14 +155,16 @@ int main() {
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init("#version 330");
 
-	// stores variables from ui input
+	// temporaryPlanet is a variable that contains the information the user is
+	// entering into the ui when the user presses "add planet" the data from temporaryPlanet 
+	// is added to the planets array and temporaryPlanet's data is reset
 	planet temporaryPlanet;
 	// center light source
 	planet sun;
-	sun.colour[0] = 1; sun.colour[1] = 0.9; sun.colour[2] = 0.1;
-	sun.mass = 10; sun.pos = glm::vec3(0, 0, 0);
+	sun.colour[0] = 1; sun.colour[1] = 0.95; sun.colour[2] = 0.1;
+	sun.mass = 50; sun.pos = glm::vec3(0, 0, 0);
 
-	// Matrices
+	// matrices
 	glm::mat4 projection = glm::perspective(glm::radians(camera.ZOOM), (float)SCRN_WIDTH / (float)SCRN_HEIGHT, 0.1f, 100.0f);
 	glm::mat4 view = camera.GetViewMatrix();
 
@@ -181,15 +184,17 @@ int main() {
 
 		// only call physics if simulation isnt paused
 		if (!paused)
-			physics(planets, sun, deltaTime, gravity, timeStep);
-
-		camera.keyboardInput(window, deltaTime);
+			physics(planets, sun, deltaTime, gravity, timeStep);;
 		
-		// Background is slightly lit by light source
-		glClearColor(sun.colour[0] * 0.08, sun.colour[1] * 0.08, sun.colour[2] * 0.08, 1);
-		//glClearColor(0.1, 0.1, 0.1, 0.1);
+		// glClearColor sets the entire colour buffer to one colour
+		// background is slightly lit by light source
+		glClearColor(sun.colour[0] * 0.06 + 0.025, sun.colour[1] * 0.06 + 0.025, sun.colour[2] * 0.06 + 0.025, 1);
+
+		// also clear the other buffers
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
+		// this matrix represents the translation, scale and rotation of what
+		// ever is currently being drawn
 		glm::mat4 model = glm::mat4(1.0);
 
 		shaders.use();
@@ -197,35 +202,83 @@ int main() {
 		// draw light in middle
 		model = glm::scale(model, glm::vec3(0.05));
 
+		// send shader variables to GPU as "unifroms"
 		shaderInputs(shaders, model, view, projection, 
 			glm::vec4(sun.colour[0], sun.colour[1], sun.colour[2], 1),
 			glm::vec3(sun.colour[0], sun.colour[1], sun.colour[2]),
 			true);
 
-		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glBindVertexArray(VAO);
+		// this is a draw call for openGL
 		glDrawArrays(GL_TRIANGLES, 0, vertices.size() * sizeof(float));
 
-		// call addPlanet to get how many objects there are
+		// loop through each planet in planet array
 		for (int i = 0; i < planets.size(); i++) {
+			// reset the model matrix
 			model = glm::mat4(1.0);
 			model = glm::scale(model, glm::vec3(0.05));
 			model = glm::translate(model, planets[i].pos);
-
-			printf("%f %f\n", planets[i].pos.x, planets[i].pos.y);
 
 			shaderInputs(shaders, model, view, projection,
 				glm::vec4(planets[i].colour[0], planets[i].colour[1], planets[i].colour[2], 1),
 				glm::vec3(sun.colour[0], sun.colour[1], sun.colour[2]),
 				true);
 
-			//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 			glBindVertexArray(VAO);
 			glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+
+			if (showVectors) {
+				// draw force vectors to other objects
+				arrowShader.use();
+
+				model = glm::mat4(1);
+				// vector towards sun
+				model = glm::scale(model, glm::vec3(0.05, 0.05, 0.05));
+				model = glm::translate(model, planets[i].pos);
+				model = glm::rotate(model, generateAngle(-planets[i].pos.x, -planets[i].pos.y), glm::vec3(0, 0, -1));
+
+				shaderInputs(shaders, model, view, projection,
+					glm::vec4(planets[i].colour[0], planets[i].colour[1], planets[i].colour[2], 1),
+					glm::vec3(1, 1, 1),
+					false);
+
+				glBindVertexArray(VAO2);
+				glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+
+				// velocity vector of current planet
+				model = glm::mat4(1);
+				model = glm::scale(model, glm::vec3(0.05, 0.05, 0.05));
+				model = glm::translate(model, planets[i].pos);
+				model = glm::rotate(model, generateAngle(planets[i].velocity.x, planets[i].velocity.y), glm::vec3(0, 0, -1));
+
+				shaderInputs(shaders, model, view, projection,
+					glm::vec4(planets[i].colour[0], planets[i].colour[1], planets[i].colour[2], 1),
+					glm::vec3(1, 1, 1),
+					false);
+
+				glBindVertexArray(VAO2);
+				glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+
+				// force vector from current vector towards other planets
+				for (int j = 0; j < planets.size(); j++)
+					if (j != i) {
+						model = glm::mat4(1);
+						model = glm::scale(model, glm::vec3(0.05, 0.05, 0.05));
+						model = glm::translate(model, planets[i].pos);
+						model = glm::rotate(model, generateAngle(planets[j].pos.x - planets[i].pos.x, planets[j].pos.y - planets[i].pos.y), glm::vec3(0, 0, -1));
+
+						shaderInputs(shaders, model, view, projection,
+							glm::vec4(planets[i].colour[0], planets[i].colour[1], planets[i].colour[2], 1),
+							glm::vec3(1, 1, 1),
+							false);
+
+						glBindVertexArray(VAO2);
+						glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+				}
+			}
 		}
 
-		// draw ui to screen
-		// tell imgui its new frame
+		// ui boiler plate code
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
@@ -235,7 +288,6 @@ int main() {
 		ImGui::Text("---PLANET CREATE---");
 		ImGui::Text("Planet Colour");
 		ImGui::ColorPicker3("", temporaryPlanet.colour);
-		//ImGui::InputText("Planet name", temporaryPlanet.name, 512);
 		ImGui::Text("Planet Mass");
 		ImGui::SliderFloat("1 - 10", &temporaryPlanet.mass, 1, 10);
 		ImGui::Text("Starting Velocity");
@@ -253,51 +305,59 @@ int main() {
 
 		//ImGui::Text(" ");
 		ImGui::Text("---SIMULATION VARIABLES---");
-		// if not paused display paused button
-		if (!paused)
-			if (ImGui::Button("Pause Simulation"))
-				paused = true;
-		// if paused display continue button
-		if (paused)
-			if (ImGui::Button("Play Simulation"))
-				paused = false;
+
+		// if not paused, display paused button
+		if (!paused && ImGui::Button("Pause Simulation"))
+			paused = true;
+		// if paused, display continue button
+		if (paused && ImGui::Button("Play Simulation"))
+			paused = false;
+
+		if (ImGui::Button("Show Vectors"))
+			showVectors = !showVectors;
 
 		ImGui::Text("Strength Of Gravity");
 		ImGui::SliderFloat("0.005 - 1", &gravity, 0.005, 1);
 		ImGui::Text("Timestep Of Simulation");
 		ImGui::SliderFloat("0.25 - 3", &timeStep, 0.25, 3);
+		ImGui::Text("Sun Mass");
+		ImGui::SliderFloat("10 - 100", &sun.mass, 10, 100);
 		ImGui::Text("Light Colour");
 		ImGui::ColorPicker3(" ", sun.colour);
 
-
+		// return mouse position and screen size
 		glfwGetCursorPos(window, &mxpos, &mypos);
 		glfwGetWindowSize(window, &scrnX, &scrnY);
 		
-		// normalized mouse coords
+		// mxpos is mouse x position, mypos is mouse y position
+		// this line makes mxpos and mypos a percentage
 		mxpos /= scrnX; mypos /= scrnY; 
-		// between -0.5 and 0.5
+		// if mypos = 1, the mouse is at the top of the screen
+		// if mypos = 0, the mouse is at the bottom of the screen
+
+		// now mxpos and mypos go between -0.5 and 0.5
 		mxpos -= 0.5; mypos -= 0.5;
-		// *2 to scale from -1 to 1
+		// now mxpos and mypos go from -1 to 1
 		mxpos *= 2; mypos *= -2;
 		
-		// render semi transparent planet if user is choosing position of new planet
+		// if the user is selecting the planet position, draw a planet at the mouse coordinates
 		if (choosePlanetPos) {
 			shaders.use();
 
-			// check to place planet
+			// if the user clicks set the mouse coordinates as the planet position
 			if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS && !positionChoosen) {
 				positionChoosen = true;
 				temporaryPlanet.pos = glm::vec3(mxpos * _simWidth, mypos * _simHeight, 0);
-				// coordinate from -1 to 1 of placed planets position
-				normPlacedPosition.x = mxpos; normPlacedPosition.y = mypos;
+				// record what the time was when the user clicked
 				timeAtClick = glfwGetTime();
 			}
 
 			model = glm::mat4(1);
 			model = glm::scale(model, glm::vec3(0.05, 0.05, 0.05));
-			// translate to mouse pos if position isnt choosen
+			// move the temporary planet to mouse pos if position isnt choosen
 			if (!positionChoosen)
 				model = glm::translate(model, glm::vec3(mxpos * _simWidth, mypos * _simHeight, 0));
+			// if the position is choosen dont move the temporary planet
 			else
 				model = glm::translate(model, temporaryPlanet.pos);
 			
@@ -309,14 +369,17 @@ int main() {
 			glBindVertexArray(VAO);
 			glDrawArrays(GL_TRIANGLES, 0, vertices.size());
 
-			// once position is chossen draw arrow showing starting velocity
+			// once position is chossen draw arrow showing starting velocity the points towards users mouse
 			if (positionChoosen) {
-				// if Add Planet is clicked and all planet values are filled add the planet
+				// if "add planet" is clicked and all planet values are entered, add the planet
 				if (addPlanetClicked && velocityChoosen && checkPlanet(temporaryPlanet)) {
 					choosePlanetPos = false;
 					positionChoosen = false;
+					velocityChoosen = false;
+					// this only normalizes the x and y component of vector
 					normalize(temporaryVector);
 					// add velocity to temporary planet
+					// temporaryVector.z contains the scale of the vector
 					temporaryPlanet.velocity.x = temporaryVector.x * temporaryVector.z;
 					temporaryPlanet.velocity.y = temporaryVector.y * temporaryVector.z;
 
@@ -329,12 +392,16 @@ int main() {
 				}
 
 				if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS &&
-					glfwGetTime() - timeAtClick > 0.5) {
+					// add time delay to avoid the user having the mouse clicked for more than one frame
+					// resulting in multiple clicks registered when the user only wanted to click once
+					glfwGetTime() - timeAtClick > 0.25 &&
+					!velocityChoosen) {
 					velocityChoosen = true;
 					temporaryVector.x = mxpos * _simWidth - temporaryPlanet.pos.x;
 					temporaryVector.y = mypos * _simHeight - temporaryPlanet.pos.y;
 				}
 
+				// now draw an arrow that points towards users mouse to show starting velocity
 				arrowShader.use();
 				// draw arrow for starting velocity
 				model = glm::mat4(1);
@@ -371,30 +438,4 @@ int main() {
 	glfwTerminate();
 
 	return 0;
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
-}
-
-
-void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
-{
-	float xpos = static_cast<float>(xposIn);
-	float ypos = static_cast<float>(yposIn);
-
-	if (firstMouse)
-	{
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
-	}
-
-	float xoffset = xpos - lastX;
-	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
-	lastX = xpos;
-	lastY = ypos;
-
-	//camera.mouseInput(xoffset, yoffset);
 }
